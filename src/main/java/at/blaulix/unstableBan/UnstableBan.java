5197bf8b35bea76011513f0cdbf932e7419be88a
blaulix <blaulix@gmx.at>
2026-01-27 13:12:26 +0100

    Rename saveBans interface to saveReadMethods and add banCount method for retrieving ban count

--- src/main/java/at/blaulix/unstableBan/UnstableBan.java
+++ src/main/java/at/blaulix/unstableBan/UnstableBan.java
@@ -1,20 +1,23 @@
 package at.blaulix.unstableBan;
 
 import org.bukkit.Bukkit;
+import org.bukkit.MinecraftExperimental;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FireworkExplodeEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.UUID;
 
-public final class UnstableBan extends JavaPlugin implements Listener, saveBans {
+public final class UnstableBan extends JavaPlugin implements Listener, saveReadMethods {
     private File banFile;
     private FileConfiguration banConfig;
 
@@ -36,25 +39,38 @@ public final class UnstableBan extends JavaPlugin implements Listener, saveBans 
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        try{
+            savebansfile(banFile, banConfig);
+            getLogger().info("UnstableBan saved config and disabled (v"+ getPluginMeta().getVersion() +")");
+        } catch (Exception e) {
+            getLogger().info("ERROR while saving config on disable!");
+        }
     }
 
     @EventHandler
     public void onFirstJoin(PlayerJoinEvent event){
         Player player = event.getPlayer();
-        if (!player.hasPlayedBefore() || banConfig.getInt("bans." + player.getUniqueId().toString() + ".banCount"){
-            String uuid = player.getUniqueId().toString();
+        UUID uuid = player.getUniqueId();
+        int banTimesLength = getConfig().getStringList("ban-times").size();
+
+        if (!player.hasPlayedBefore() || banCount(banConfig, uuid) == banTimesLength){
 
             banConfig.set("bans." + uuid + ".banCount", 0);
             savebansfile(banFile, banConfig);
         }
     }
 
+    @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event){
         Player player = event.getPlayer();
         Entity killer = player.getKiller();
         if (killer instanceof Player){
-            Bukkit.getConsoleSender();
+            UUID uuid = player.getUniqueId();
+            int currentBanCount = banCount(banConfig, uuid);
+            String banTime = getConfig().getStringList("ban-times").get(currentBanCount);
+            Bukkit.dispatchCommand(
+                    Bukkit.getConsoleSender(), "ban" + uuid + " " + banTime + " You got killed banned by " + killer.getName()
+            );
         }
     }
 }
--- src/main/java/at/blaulix/unstableBan/saveReadMethods.java
+++ src/main/java/at/blaulix/unstableBan/saveReadMethods.java
@@ -3,13 +3,18 @@ package at.blaulix.unstableBan;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import java.io.File;
+import java.util.UUID;
 
-public interface saveBans {
+public interface saveReadMethods {
     default void savebansfile(File banFile, FileConfiguration banConfig) {
         try {
             banConfig.save(banFile);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    default int banCount(FileConfiguration banConfig, UUID uuid) {
+        return banConfig.getInt("bans." + uuid + ".banCount");
     }
 }
